2b9f225b22f535ef4af3cabc6161fc9733e167e4
ga1duk <gaidukdg@yandex.ru>
2022-02-12 17:12:31 +0300

    README.md was added. Post.kt file was added. It is now possible to retrieve all posts from the server

---
+++ README.md
@@ -0,0 +1,2 @@
+# Домашнее задание к занятию «3.1 Coroutines: легковесные потоки, Thread Pools & Dispatchers»
+## Задача authorId
---
+++ app/src/main/java/ru/netology/authorid/Post.kt
@@ -0,0 +1,38 @@
+package ru.netology.authorid
+
+data class Post(
+    val id: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0,
+    val attachment: Attachment? = null
+)
+
+data class Attachment(
+    val url: String,
+    val description: String,
+    val type: AttachmentType
+)
+
+data class Comment(
+    val id: Long,
+    val postId: Long,
+    val authorId: Long,
+    val content: String,
+    val published: Long,
+    val likedByMe: Boolean,
+    val likes: Int = 0
+)
+
+data class Author(
+    val id: Long,
+    val name: String,
+    val avatar: String
+)
+
+enum class AttachmentType {
+    IMAGE
+}
+
